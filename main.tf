0584142612a11f64ef42c29008652931d12c7cda
Les Aker <me@lesaker.org>
2019-04-05 09:34:47 +0100

    update to latest base

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ data "template_file" "config" {
 
 module "vm" {
   source          = "armorfret/wireguard-base/linode"
-  version         = "0.0.6"
+  version         = "0.0.7"
   name            = "${var.name}"
   ssh_keys        = ["${var.ssh_keys}"]
   ssh_users       = ["${var.ssh_users}"]
