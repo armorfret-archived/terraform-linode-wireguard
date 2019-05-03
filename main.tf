3b761e1215ac5b0e23888a5854ecff9925ea93c8
Les Aker <me@lesaker.org>
2019-05-03 10:57:31 +0100

    switch to stackscript

--- main.tf
+++ main.tf
@@ -1,65 +1,39 @@
-data "template_file" "users" {
-  template = "${file("${path.module}/assets/users.cfg")}"
+resource "linode_instance" "vpn" {
+  label = "${var.name}-wg"
 
-  vars {
-    users = "${jsonencode(var.users)}"
-  }
-}
+  region = "${var.region}"
+  type   = "${var.type}"
 
-resource "local_file" "users" {
-  content  = "${data.template_file.users.rendered}"
-  filename = "${path.root}/.configs/${var.name}.cfg"
-}
+  disk {
+    label = "root"
+    size  = 10240
 
-module "vm" {
-  source            = "armorfret/wireguard-base/linode"
-  version           = "0.0.8"
-  name              = "${var.name}"
-  ssh_keys          = ["${var.ssh_keys}"]
-  ssh_users         = ["${var.ssh_users}"]
-  region            = "${var.region}"
-  type              = "${var.type}"
-  source_image_id   = "${var.image_id}"
-  ansible_repo_path = "${var.ansible_repo_path}"
-}
+    // TODO: remove this
+    authorized_keys = ["${var.ssh_keys}"]
+    image           = "${var.image_id}"
+    stackscript_id  = "${linode_stackscript.this.id}"
 
-resource "null_resource" "configuration" {
-  triggers = {
-    linode_id  = "${module.vm.linode_id}"
-    ip_address = "${module.vm.ip_address}"
-  }
-
-  connection {
-    type = "ssh"
-    user = "root"
-    host = "${module.vm.ip_address}"
-  }
-
-  provisioner "ansible" {
-    plays {
-      playbook = {
-        file_path = "${var.ansible_repo_path}/main.yml"
-        tags      = ["users"]
-      }
+    stackscript_data = {
+      "users"       = "${var.users}"
+      "deploy_repo" = "${var.deploy_repo}"
     }
+  }
 
-    defaults {
-      extra_vars = {
-        ansible_python_interpreter = "/usr/bin/python3"
-        wireguard_config_path      = "/opt/wireguard"
-        user_config_file           = "${local_file.users.filename}"
+  config {
+    label  = "default"
+    kernel = "linode/grub2"
+
+    devices {
+      sda = {
+        disk_label = "root"
       }
     }
   }
+}
 
-  provisioner "local-exec" {
-    command     = "${path.module}/assets/download.rb"
-    working_dir = "${path.root}"
-
-    environment = {
-      VPN_IP_ADDRESS = "${module.vm.ip_address}"
-      VPN_NAME       = "${var.name}"
-      VPN_REGION     = "${var.region}"
-    }
-  }
+resource "linode_stackscript" "this" {
+  label       = "${var.name}-wg"
+  description = "Deploy wireguard"
+  script      = "${file("${path.module}/assets/stackscript.sh")}"
+  images      = ["${var.image_id}"]
 }
