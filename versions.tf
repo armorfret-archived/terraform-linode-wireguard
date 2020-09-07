c720711e6f28f12800d0fb26f0e911f3f0355e52
Les Aker <me@lesaker.org>
2020-09-07 16:07:33 +0000

    update providers

--- versions.tf
+++ versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     linode = {
-      source = "terraform-providers/linode"
+      source  = "linode/linode"
+      version = "1.12.4"
     }
   }
-  required_version = ">= 0.13"
 }
